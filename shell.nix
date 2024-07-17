5f74f4b513c32c01d7a8a462117e75004d7e89a6
psychosis448 <psychosis448@protonmail.com>
2024-07-17 15:21:06 +0200

    Fix go testing cmd

--- shell.nix
+++ shell.nix
@@ -6,7 +6,7 @@
         name = "gt";
         category = "testing";
         help = "Test go code";
-        command = "go test -v";
+        command = "go test -v ./...";
       }
       {
         name = "fmt";
@@ -20,6 +20,7 @@
     ## https://blog.mplanchard.com/posts/installing-a-specific-version-of-a-package-with-nix.html
     packages = with pkgs; [
       go
+      golines
       gopls
       gotools
     ];
