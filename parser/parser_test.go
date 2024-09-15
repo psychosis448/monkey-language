0c2ca8dd7e1f4fae437ff0a319c39242473e03c7
psychosis448 <psychosis448@protonmail.com>
2024-09-15 20:42:13 +0200

    Add parser skeleton
    Adds a basic skeleton for the parser, as well as an initial test case.

---
+++ parser/parser_test.go
@@ -0,0 +1,72 @@
+package parser
+
+import (
+	"psychosis448/monkey/ast"
+	"psychosis448/monkey/lexer"
+	"testing"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+    let x = 5;
+    let y = 10;
+    let foobar = 838383;
+    `
+
+    // Actual usage of lexer, instead of mock, might blow up parser tests, that
+    // are actually bugs in the lexer.
+    // But this trade-off enables us to use actual source code as input here,
+    // which increases readability!
+	l := lexer.New(input)
+	p := New(l)
+
+	program := p.ParseProgram()
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d",
+			len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
+		return false
+	}
+
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("s not *ast.LetStatement. got=%T", s)
+		return false
+	}
+
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+		return false
+	}
+
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("letStmt.Name.TokenLiteral() not '%s'. got=%s", name, letStmt.Name.TokenLiteral())
+		return false
+	}
+
+	return true
+}
